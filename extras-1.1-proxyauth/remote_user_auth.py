c7ac6611c5648dc0a09a06640ea023d7d6023655
Staffan Olsson <staffan@repos.se>
2016-04-13 10:18:24 +0200

    Yet another reason that this plugin must be site specific
    No user got any privilege, resulting in WIKI_VIEW forbidden,
    when authenticate returned a username with @ in it.
    This sample replacements returns a simple short username convention.

--- extras-1.1-proxyauth/remote_user_auth.py
+++ extras-1.1-proxyauth/remote_user_auth.py
@@ -1,6 +1,7 @@
 from trac.core import *
 from trac.config import BoolOption
 from trac.web.api import IAuthenticator
+import re
 
 class MyRemoteUserAuthenticator(Component):
 
@@ -9,7 +10,14 @@ class MyRemoteUserAuthenticator(Component):
     auth_header = Option('trac', 'remote_user_auth_header', '',
                """Trust this HTTP header for login""")
 
+    # trac's permission system, at least in 1.1, bails out on @ in username
+    authmail = re.compile(r'^(.)[^\.]*\.([^@]*).*$', re.IGNORECASE)
+
     def authenticate(self, req):
         if self.auth_header and req.get_header(self.auth_header):
-            return req.get_header(self.auth_header)
+            account = req.get_header(self.auth_header)
+            tracsafe = self.authmail.sub('\\1\\2',account)
+            if not tracsafe:
+                return account
+            return tracsafe
         return None
